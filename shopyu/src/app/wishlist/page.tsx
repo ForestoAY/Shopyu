10ffe14ec0ab48c5f6a5e2fadafcfdaec632da5e
ForestoAY <forestoay@gmail.com>
2024-09-23 20:50:54 +0700

    feat: add wishlistCard components

--- shopyu/src/app/wishlist/page.tsx
+++ shopyu/src/app/wishlist/page.tsx
@@ -1,3 +1,4 @@
+import WishlistCard from "@/components/WishlistCard";
 import { Product } from "../products/page";
 
 export default async function WishlistPage() {
@@ -11,26 +12,8 @@ export default async function WishlistPage() {
         <p>Your wishlist is empty.</p>
       ) : (
         <div className="space-y-4">
-          {wishlist.map((product, index) => (
-            <div key={index} className="flex items-center border-b pb-4">
-              <div className="flex-shrink-0">
-                <img
-                  src={product.thumbnail}
-                  alt={product.name}
-                  className="w-20 h-20 object-cover rounded"
-                />
-              </div>
-              <div className="ml-4 flex-grow">
-                <h2 className="text-lg font-semibold">{product.name}</h2>
-                <p className="text-gray-600">{product.description}</p>
-                <p className="text-lg font-bold text-red-500">
-                  Price: {product.price.toLocaleString()} IDR
-                </p>
-              </div>
-              <div className="flex-shrink-0">
-                <button className="btn btn-primary">Remove</button>
-              </div>
-            </div>
+          {wishlist.map((e, i) => (
+            <WishlistCard key={i} product={e} />
           ))}
         </div>
       )}
---
+++ shopyu/src/components/WishlistCard.tsx
@@ -0,0 +1,25 @@
+import { Product } from "@/app/products/page";
+
+export default function WishlistCard({ product }: { product: Product }) {
+  return (
+    <div className="flex items-center border-b pb-4">
+      <div className="flex-shrink-0">
+        <img
+          src={product.thumbnail}
+          alt={product.name}
+          className="w-20 h-20 object-cover rounded"
+        />
+      </div>
+      <div className="ml-4 flex-grow">
+        <h2 className="text-lg font-semibold">{product.name}</h2>
+        <p className="text-gray-600">{product.description}</p>
+        <p className="text-lg font-bold text-red-500">
+          Price: {product.price.toLocaleString()} IDR
+        </p>
+      </div>
+      <div className="flex-shrink-0">
+        <button className="btn btn-primary">Remove</button>
+      </div>
+    </div>
+  );
+}
